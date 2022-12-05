d753154f6464f56cfde7ba17fa3b0ef3cda67ccc
Saraesp <saraesposito145@gmail.com>
2022-12-05 16:30:20 +0100

    style.css della lista e fine js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,16 @@ const lista = [
     'Formaggio',
     'Pizza Surgelata'
 ]
+
+const listContainer = document.querySelector('.lista-spesa')
+
+let i = 0;
+let element = '';
+while(i < lista.length){
+    let item = lista[i];
+
+    let element = `<li class="list-item">${item}</li>`;
+    listContainer.innerHTML += element;
+
+    i++;
+}
